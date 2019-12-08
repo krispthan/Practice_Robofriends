16239ea60089feffc26a8148a35bb5c65c258eb1
Alex Johnson <AleJohnson@loandepot.com>
2019-12-07 19:48:11 -0800

    Updates

---
+++ precache-manifest.e07b8ea3a052111d161e2e7b21809c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78949c62bb0fa6f2e3a23fccaacc02d9",
+    "url": "/Practice_Robofriends/index.html"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/Practice_Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b008782dc91448a7baeb",
+    "url": "/Practice_Robofriends/static/css/main.1494a4bb.chunk.css"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/Practice_Robofriends/static/js/2.38e71f33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Practice_Robofriends/static/js/2.38e71f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b008782dc91448a7baeb",
+    "url": "/Practice_Robofriends/static/js/main.1aac2b4a.chunk.js"
+  },
+  {
+    "revision": "d70dbc7e7817b3815792",
+    "url": "/Practice_Robofriends/static/js/runtime-main.bf5b33ab.js"
+  }
+]);
